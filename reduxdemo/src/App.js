69292882b7803827f6b2f9b64aca08b02409eee6
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-07-27 10:28:27 +0530

    Action payload

--- reduxdemo/src/App.js
+++ reduxdemo/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import CakeCOntainer from './components/CakeCOntainer';
 import { Provider } from 'react-redux';
 import store from './components/redux/Store';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/iceCreamContainer';
+import NewCakeContainer from './components/NewCakeContainer';
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
         <CakeCOntainer />
         <HooksCakeContainer />
         <IceCreamContainer />
+        <NewCakeContainer />
       </div>
     </Provider>
   );
---
+++ reduxdemo/src/components/NewCakeContainer.js
@@ -0,0 +1,28 @@
+import React,{useState} from 'react'
+import { connect } from 'react-redux'
+import { buyCake } from './redux/cake/cakeAction'
+
+function NewCakeContainer(props) {
+    const [number, setNumber] = useState(1) 
+    return (
+        <div>
+            <h2>Number of Cakes - {props.numOfCake}</h2>
+            <input type="text" value={number} onChange={e=>setNumber(e.target.value)}/>
+            <button onClick={()=>props.buyCake(number)}>Buy {number} </button>
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        numOfCake: state.cake.numOfCake
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        buyCake: (number) => dispatch(buyCake(number))
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(NewCakeContainer)
